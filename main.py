c213b2a52009e5ed2d1267be1810490d40e63d19
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2022-04-03 13:23:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+from datetime import datetime, timedelta
+from typing import List
 from frc_py import FRC_PY
 
 
 
-api = FRC_PY()
+def get_mn(api: FRC_PY) -> List[str]:
+    raw_data = api._load('temp-cache', 'mn.json')
+    if raw_data is None or raw_data[0] < datetime.utcnow() - datetime.timedelta(days=280): # TODO
+        teams = api.get_team_index()
+        mn = []
+        for team in teams:
+            city, state_prov, country = api.get_team_location(team)
+            if state_prov == 'Minnesota' or state_prov == 'MN':
+                mn.append(team)
+        api._save('temp-cache', 'mn.json', mn)
+        return mn
+    return raw_data[1]
 
-teams = api.get_team_index()
-
-for team in teams[:10]:
-    years = api.get_team_participation(team)
-    for year in years:
-        events = api.get_team_events_year(team, year)
-        stats = api.get_team_year_stats(team, year)
-        print(f"{team} {year}:\n\t- {events}\n\t- {stats}")
 
 
+if __name__ == '__main__':
+    api = FRC_PY()
+    mn = get_mn(api)
+    print(f"{len(mn)} teams in MN")
