4385763957c255a4388891f5f0da43baf62de4d2
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2022-06-04 13:18:33 +0000

    Update example for new api

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 import os
 from frc_py import FRC_PY
+from frc_py.models import TeamSimple, Team, EventSimple, MatchSimple
 
 
 
@@ -8,16 +9,18 @@ if __name__ == '__main__':
     api = FRC_PY(TOKEN)
 
     print('***** Teams *****')
-    teams = api.get_teams()
+    teams: list[str] = api.get_teams()
     print(f"Found {len(teams)} teams")
     print(f"Years: {api.get_team_participation('frc2501')}")
-    print(f"Location: {api.get_team_location('frc2501')}")
-    print(f"Nickname: {api.get_team_nickname('frc2501')}")
-    print(f"Name: {api.get_team_name('frc2501')}")
-    print(f"School: {api.get_team_school('frc2501')}")
-    print(f"Website: {api.get_team_website('frc2501')}")
-    print(f"Rookie Year: {api.get_team_rookie_year('frc2501')}")
-    print(f"Motto: {api.get_team_motto('frc2501')}")
+    team_simple: TeamSimple = api.team_simple('frc2501')
+    print(f"Location: {team_simple.get_location()}")
+    print(f"Nickname: {team_simple.get_nickname()}")
+    print(f"Name: {team_simple.get_name()}")
+    team: Team = api.team('frc2501')
+    print(f"School: {team.school_name()}")
+    print(f"Website: {team.website()}")
+    print(f"Rookie Year: {team.rookie_year()}")
+    print(f"Motto: {team.motto()}")
     for year in api.get_team_participation('frc2501'):
         print(f"{year}: {api.get_team_events_year('frc2501', year)}")
 
@@ -25,11 +28,12 @@ if __name__ == '__main__':
     min_year, max_year = api.get_year_range()
     for year in range(min_year, max_year + 1):
         print(f"{year}: {len(api.get_events_year(year))}")
-    print(f"Name: {api.get_event_name('2022mndu')}")
-    print(f"Location: {api.get_event_location('2022mndu')}")
-    print(f"Type: {FRC_PY.event_type_to_str(api.get_event_type('2022mndu'))}")
-    print(f"Dates: {api.get_event_dates('2022mndu')}")
-    print(f"District: {api.get_event_district('2022mndu')}")
+    event_simple: EventSimple = api.event_simple('2022mndu')
+    print(f"Name: {event_simple.name()}")
+    print(f"Location: {event_simple.location()}")
+    print(f"Type: {FRC_PY.event_type_to_str(event_simple.event_type())}")
+    print(f"Dates: {event_simple.dates()}")
+    print(f"District: {event_simple.district_key()}")
     print(f"Teams: {len(api.get_event_teams('2022mndu'))}")
     print(f"Matches: {len(api.get_event_matches('2022mndu'))}")
 
@@ -37,12 +41,13 @@ if __name__ == '__main__':
     print(f"Matches: {len(api.get_team_event_matches('frc2501', '2022mndu'))}")
 
     print('***** Matches *****')
-    print(f"Level: {api.get_match_level('2022mndu_qm48')}")
-    print(f"Set Number: {api.get_match_set_number('2022mndu_qm48')}")
-    print(f"Match Number: {api.get_match_number('2022mndu_qm48')}")
-    print(f"Winner: {api.get_match_winner('2022mndu_qm48')}")
-    print(f"Red Score: {api.get_match_red_score('2022mndu_qm48')}")
-    print(f"Blue Score: {api.get_match_blue_score('2022mndu_qm48')}")
-    print(f"Red Alliance: {api.get_match_red_teams('2022mndu_qm48')}")
-    print(f"Blue Alliance: {api.get_match_blue_teams('2022mndu_qm48')}")
-    print(f"Time: {api.get_match_time('2022mndu_qm48')}")
+    match_simple: MatchSimple = api.match_simple('2022mndu_qm48')
+    print(f"Level: {match_simple.level()}")
+    print(f"Set Number: {match_simple.set_number()}")
+    print(f"Match Number: {match_simple.match_number()}")
+    print(f"Winner: {match_simple.winner()}")
+    print(f"Red Score: {match_simple.red_score()}")
+    print(f"Blue Score: {match_simple.blue_score()}")
+    print(f"Red Alliance: {match_simple.red_teams()}")
+    print(f"Blue Alliance: {match_simple.blue_teams()}")
+    print(f"Time: {match_simple.schedule_time(), match_simple.predicted_time(), match_simple.actual_time()}")
